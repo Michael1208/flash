1467c5f97cfa3c9defc0514a0a0e5601e4a54414
Michael1208 <53876489+Michael1208@users.noreply.github.com>
2019-10-10 08:33:51 -0400

    Update Flash.py

--- Flash.py
+++ Flash.py
@@ -243,7 +243,7 @@ async def serverinfo(ctx):
     await ctx.send(embed=embed)  
 	
 @bot.command()
-@has_permission(manage_messages=True)
+@commands.has_permissions(manage_messages=True)
 async def warn(ctx, person: discord.Member, *, reason = None):
   if reason == None:
       return
