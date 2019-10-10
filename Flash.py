e3afb4c785c58f8de0a18b52802f112288430517
Michael1208 <53876489+Michael1208@users.noreply.github.com>
2019-10-10 11:19:06 -0400

    Update Flash.py

--- Flash.py
+++ Flash.py
@@ -248,5 +248,48 @@ async def warn(ctx, user: discord.Member, *, msg):
     await dm.send(f"You have been warned in {ctx.guild.name} for: {msg}") 
     await ctx.send(f"{user.mention} has been warned for: {msg}")
     await ctx.message.delete()
+	
+@bot. command()
+async def eval_fn(ctx, *, cmd):
+    """Evaluates input.
+    Input is interpreted as newline seperated statements.
+    If the last statement is an expression, that is the return value.
+    Usable globals:
+      - `bot`: the bot instance
+      - `discord`: the discord module
+      - `commands`: the discord.ext.commands module
+      - `ctx`: the invokation context
+      - `__import__`: the builtin `__import__` function
+    Such that `>eval 1 + 1` gives `2` as the result.
+    The following invokation will cause the bot to send the text '9'
+    to the channel of invokation and return '3' as the result of evaluating
+    
+    """
+    fn_name = "_eval_expr"
+
+    cmd = cmd.strip("` ")
+
+    # add a layer of indentation
+    cmd = "\n".join(f"    {i}" for i in cmd.splitlines())
+
+    # wrap in async def body
+    body = f"async def {fn_name}():\n{cmd}"
+
+    parsed = ast.parse(body)
+    body = parsed.body[0].body
+
+    insert_returns(body)
+
+    env = {
+        'bot': ctx.bot,
+        'discord': discord,
+        'commands': commands,
+        'ctx': ctx,
+        '__import__': __import__
+    }
+    exec(compile(parsed, filename="<ast>", mode="exec"), env)
+
+    result = (await eval(f"{fn_name}()", env))
+    await ctx.send(result)
 
 bot.run(TOKEN)
